a034b88b90651b2163abff0b165e8aa4c7e72a7f
Lamine Toure <laminetoure@free.fr>
2025-05-16 11:47:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,15 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0"
+      version = "3.5.0"
     }
   }
   required_version = ">= 1.3.0"
 }
 
-provider "docker" {}
+provider "docker" {
+   host = "unix:///var/run/docker.sock"
+}
 
 module "network" {
   source = "./modules/network"
